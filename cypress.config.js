89ddbe0f6307b4ed3c29341dcc809ae14438e51b
guilhermevigneron <guilhermevigneron@gmail.com>
2025-07-05 10:11:02 -0500

    refactor: Remove cypress-audit from Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,7 @@
 const { defineConfig } = require('cypress');
-const { lighthouse, prepareAudit } = require('cypress-audit');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:4000',
-    setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        prepareAudit(launchOptions);
-      });
-
-      on('task', {
-        lighthouse: lighthouse(),
-      });
-    },
   },
 });
