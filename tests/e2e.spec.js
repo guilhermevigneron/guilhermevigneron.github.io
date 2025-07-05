11a7f604a59c3d3bf05178f77d63d17c4b463b3b
guilhermevigneron <guilhermevigneron@gmail.com>
2025-07-05 10:24:37 -0500

    feat: Add Cypress e2e tests for homepage

---
+++ tests/e2e.spec.js
@@ -0,0 +1,29 @@
+describe('Homepage', () => {
+  beforeEach(() => {
+    // Visit the base URL defined in cypress.config.js
+    cy.visit('/');
+    // Inject the axe-core library for accessibility checks
+    cy.injectAxe();
+  });
+
+  it('should display the correct name in the header', () => {
+    cy.get('h1').should('contain.text', 'Guilherme Vigneron de Oliveira');
+  });
+
+  it('should have visible navigation links', () => {
+    cy.get('a[href="https://www.linkedin.com/in/guilherme-vigneron-de-oliveira"]').should('be.visible');
+    cy.get('a[href="https://github.com/guilhermevigneron"]').should('be.visible');
+    cy.get('a[href="mailto:guilhermevigneron@gmail.com"]').should('be.visible');
+  });
+
+  it('should have the main sections visible', () => {
+    cy.contains('h2', 'About Me').should('be.visible');
+    cy.contains('h2', 'Professional Experience').should('be.visible');
+    cy.contains('h2', 'Core Competencies').should('be.visible');
+  });
+
+  it('should have no accessibility violations on load', () => {
+    // Run accessibility check on the entire page
+    cy.checkA11y();
+  });
+});
